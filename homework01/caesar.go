0ecbfb59275ec987b2b276e4d95e89d7c4cc91f6
Anna <anna-maks99@lenta.ru>
2018-10-18 18:46:04 +0300

    Реализована функция decrypt_caesar()

--- homework01/caesar.go
+++ homework01/caesar.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 }
+
 func encrypt_caesar(plaintext string) string {
 	word := bytes.Runes([]byte(plaintext))
 	ciphertext := ""
@@ -18,4 +19,18 @@ func encrypt_caesar(plaintext string) string {
 		ciphertext += string(word[i])
 	}
 	return ciphertext
+}
+
+func decrypt_caesar(ciphertext string) string {
+	word := bytes.Runes([]byte(ciphertext))
+	plaintext := ""
+	for i := 0; i < len(word); i++ {
+		if word[i] >= 68 && word[i] <= 90 || word[i] >= 100 && word[i] <= 122 {
+			word[i] -= 3
+		} else if word[i] >= 65 && word[i] <= 67 || word[i] >= 97 && word[i] <= 99 {
+			word[i] += 23
+		}
+		plaintext += string(word[i])
+	}
+	return plaintext
 }
