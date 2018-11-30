9ca8f02c9e3041d0bb309767c7006d7bd61be6a2
Anna <anna-maks99@lenta.ru>
2018-12-01 00:02:19 +0300

    В функцию plot_graph добавлена возможность строить граф по матрице

--- homework04/network.py
+++ homework04/network.py
@@ -47,22 +47,30 @@ def plot_graph(graph: int):
     vertices = [lastname['items'][i]['first_name']
                 + ' ' + lastname['items'][i]['last_name']
                 for i in range(lastname['count'])]
-    edges = get_network(get_friends_id(graph))
-    # Создание графа
-    g = Graph(vertex_attrs={"shape": "triangle",
-                            "label": vertices,
-                            "size": 10},
-              edges=edges, directed=False)
+    edges = get_network(get_friends_id(graph), False)
+    if isinstance(edges, ndarray):
+        g = Graph.Adjacency(edges.tolist(), mode='undirected')
+        g.vs['label'] = vertices
+        g.vs['shape'] = 'triangle'
+        g.vs['size'] = 10
+    else:
+        # Создание графа
+        g = Graph(vertex_attrs={"shape": "triangle",
+                                "label": vertices,
+                                "size": 10},
+                  edges=edges, directed=False)
 
     # Задаем стиль отображения графа
     n = len(vertices)
     visual_style = {"vertex_size": 20,
-                    "bbox": (2000, 2000),
-                    "margin": 100,
+                    "bbox": (2000, 2000),  # размер поля
+                    "margin": 100,  # расстояние от края до вершин
                     "vertex_label_dist": 1.6,  # расстояние между вершинами
                     "edge_color": "gray",
-                    "autocurve": True,
+                    "autocurve": True,  # кривизна ребер
                     "layout": g.layout_fruchterman_reingold(
+                        # Fruchterman-Reingold force-directed algorithm
+                        # алгоритм компоновки
                         maxiter=1000,
                         area=n ** 3,
                         repulserad=n ** 3)}
