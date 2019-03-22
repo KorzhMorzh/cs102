025425cb7a7949d8313c759ce8750f8417f7c6a5
Anna <anna-maks99@lenta.ru>
2019-03-22 13:37:35 +0300

    Максимальная память вычисляется теперь в map()

--- homework08/pool.py
+++ homework08/pool.py
@@ -10,13 +10,31 @@ class ProcessPool:
         self.min_workers = min_workers
         self.max_workers = max_workers
         self.mem_usage = mem_usage
-        self.max_memory_ = 0
+        self.max_memory = 0
 
     def map(self, func, iterable):
+        begin = time.time()
+        max_memory_ = 0
+        start_memory = psutil.virtual_memory().used
+        memory_usage_refresh = .005
+        p = Process(target=func, args=(max(iterable),))
+        p.start()
+        while 1:
+            time.sleep(memory_usage_refresh)
+            delta_mem = psutil.virtual_memory().used - start_memory
+            if delta_mem > max_memory_:
+                max_memory_ = delta_mem
+            if not p.is_alive():
+                end = time.time()
+                p.join()
+                print("Время обработки 1 чанка данных одним процессом: ",
+                      int(end - begin))
+                self.max_memory = int(max_memory_ / 1024 / 1024)
+                break
         processes = []
         amount_process = int(int(self.mem_usage.split('Gb')[0])
-                             * 1024 / self.max_memory_)
-        if self.max_memory_ > int(self.mem_usage.split('Gb')[0])*1024:
+                             * 1024 / self.max_memory)
+        if self.max_memory > int(self.mem_usage.split('Gb')[0])*1024:
             print("Процесс занимает больше памяти, чем возможно")
             return 0
         if amount_process < self.min_workers:
@@ -43,26 +61,6 @@ class ProcessPool:
             for process in processes:
                 process.join()
 
-    def max_memory(self, func, iterable):
-        begin = time.time()
-        max_memory_ = 0
-        start_memory = psutil.virtual_memory().used
-        memory_usage_refresh = .005
-        p = Process(target=func, args=(iterable,))
-        p.start()
-        while 1:
-            time.sleep(memory_usage_refresh)
-            delta_mem = psutil.virtual_memory().used - start_memory
-            if delta_mem > max_memory_:
-                max_memory_ = delta_mem
-            if not p.is_alive():
-                end = time.time()
-                p.join()
-                print("Время обработки 1 чанка данных одним процессом: ",
-                      int(end - begin))
-                self.max_memory_ = int(max_memory_ / 1024 / 1024)
-                return self.max_memory_
-
 
 def heavy_computation(data_chunk):
     for i in range(4534545):
@@ -75,5 +73,4 @@ if __name__ == '__main__':
     pool = ProcessPool(min_workers=2, max_workers=10, mem_usage='1Gb')
     m = 50
     big_data = [random.randint(-m * m, m * m) for i in range(m)]
-    pool.max_memory(heavy_computation, max(big_data))
     result = pool.map(heavy_computation, big_data)
